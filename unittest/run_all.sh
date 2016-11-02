52ef1cb061c32949e42adcf03a1be0bc14932336
ribu <ribu@testserver.(none)>
2016-11-02 11:06:03 +0100

    Added gcov

--- unittest/run_all.sh
+++ unittest/run_all.sh
@@ -5,6 +5,7 @@ date | tee -a $OUT
 echo "" | tee -a $OUT
 ./run_normal.sh | tee -a $OUT
 ./run_sanitizer.sh | tee -a $OUT
+./run_gcov.h | tee -a $OUT
 ./run_gprof.sh | tee -a $OUT
 ./run_valgrind.sh | tee -a $OUT
 ./run_compare.sh | tee -a $OUT
